43fdbf16ce2754698c71e8e02a2fa31bc493fb8c
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2023-02-06 12:10:37 +0530

    update

--- handle_missing_values/missing_value.py
+++ handle_missing_values/missing_value.py
@@ -1,2 +1,28 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
+def drop_na(data_path):
+    """
+        delete all the rows, if all columns values are missing.
+    """
+    try:
+        data = pd.read_csv(data_path)
+        data.dropna(how='all', inplace=True)
+        return data
+    except Exception as ex:
+        print(ex)
+        raise ex   
+
+
+
+
+
+if __name__ == "__main__":
+    
+    data = drop_na('Data/missing.csv')
+    print(data)
+    print(data.isnull().sum().to_dict())
+    
